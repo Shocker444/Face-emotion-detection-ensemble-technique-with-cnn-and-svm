50bc70c9e429ea77bca66452d13820e2866c0720
Shocker444 <101675941+shocker444@users.noreply.github.com>
2024-03-26 20:16:15 +0100

    inference update

--- inference.py
+++ inference.py
@@ -1,16 +1,15 @@
+import mediapipe as mp
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras import layers, backend as K
 from model import extractor_model
-
-import numpy as np
 import cv2
-
 import joblib
 
-
+mp_face_detection = mp.solutions.face_detection
 rescale = layers.Rescaling(scale=1./127.5, offset=-1)
 classes = ['suprise', 'fear', 'disgust', 'happy','sad', 'angry', 'neutral']
+
+
 def export_model(image, extractor, classifier):
     image = rescale(image)
     layer_output = K.function(inputs = extractor.layers[0].input, outputs = extractor.layers[-5].output)
@@ -21,31 +20,51 @@ def export_model(image, extractor, classifier):
 
     return pred
 
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
 extractor = extractor_model()
 extractor.load_weights('facemodel2.h5')
 classifier = joblib.load('final_classifier4.sav')
 
+# For webcam input:
 cap = cv2.VideoCapture(0)
 
-while True:
-    ret, frame = cap.read()
+with mp_face_detection.FaceDetection(
+    model_selection=1, min_detection_confidence=1) as face_detection:
+    while cap.isOpened():
+        success, frame = cap.read()
+        if not success:
+            print("Ignoring empty camera frame.")
+                # If loading a video, use 'break' instead of 'continue'.
+            continue
 
-    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    faces = face_cascade.detectMultiScale(frame, scaleFactor=1.2, minNeighbors=3)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image = frame.copy()
+        height, width, channel = image.shape
 
-    for (x, y, w, h) in faces:
-        face = frame[y:y + h, x:x + w]
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = face_detection.process(image)
 
-        face = tf.image.resize(face, (224, 224))
-        prediction = export_model(face, extractor, classifier)
+        # Draw the face detection annotations on the image.
+        image.flags.writeable = True
+        if results.detections:
+            for detection in results.detections:
 
-        cv2.putText(frame, classes[prediction[0]], (x, y), cv2.FONT_HERSHEY_COMPLEX, .75, (255, 0, 0), thickness=2)
-        cv2.rectangle(frame, pt1=(x, y), pt2=(x + w, y + h), color=(225, 0, 0), thickness=2)
+                bb = detection.location_data.relative_bounding_box
 
-    cv2.imshow('frame', frame)
+                x, y, w, h = int(bb.xmin*width), int(bb.ymin*height), int(bb.width*width), int(bb.height*height)
+                face = image[y:y + h, x:x + w]
+                face = tf.image.resize(face, (224, 224))
+                prediction = export_model(face, extractor, classifier)
 
-    if cv2.waitKey(100) & 0xFF == ord('q'):
-        break
+                cv2.putText(frame, classes[prediction[0]], (x, y), cv2.FONT_HERSHEY_COMPLEX, .75, (255, 100, 150), thickness=2)
+                cv2.rectangle(frame, pt1=(x, y), pt2=(x + w, y + h), color=(225, 255, 255), thickness=2)
+
+
+        cv2.imshow('MediaPipe Face Detection', frame)
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+
 cap.release()
 cv2.destroyAllWindows()
